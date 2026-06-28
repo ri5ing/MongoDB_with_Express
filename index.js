8da4109bc3fe2e20b94f378f1aeb46c1aac5a3ab
ri5ing <uday.patil.st@gmail.com>
2026-06-28 19:27:37 +0530

    created chat model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
+const Chat = require("./model/chat.js");
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -14,8 +15,19 @@ main()
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-
 }
+
+let chat1 = new Chat({
+    from: "om",
+    to : "dron",
+    msg: "send me your exam notes",
+    created_at: new Date(),
+});
+
+chat1.save().then((res)=>{
+    console.log(res);
+});
+
 
 app.get ("/" , (req,res) =>  {
     res.send("root is working");
