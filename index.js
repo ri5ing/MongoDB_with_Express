cfd43646174a532b91f3af94f7d8a365108e04f9
ri5ing <uday.patil.st@gmail.com>
2026-06-28 21:06:19 +0530

    added Create Route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./model/chat.js");
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
    .then(()=> {
@@ -28,7 +29,28 @@ app.get("/chats" , async (req,res)=>{
 //New Route
 app.get("/chats/new",(req,res)=>{
     res.render("new.ejs");
-})
+});
+
+//Create Route
+app.post("/chats" , (req,res)=>{
+    let {from,to,msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to:to,
+        msg:msg,
+        created_at:new Date()
+    });
+
+    newChat
+    .save()
+    .then(res => {
+        console.log("chat was saved");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+});
 
 
 app.get ("/" , (req,res) =>  {
